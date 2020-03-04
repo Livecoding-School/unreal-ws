20d72984a55928b358981b4a24ffda94b4d9ffbf
microdee <m@mcro.de>
2020-03-04 17:28:37 +0100

    added project for 1st WS

---
+++ LcsUeWsP1/Source/LcsUeWsP1/LcsUeWsP1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LcsUeWsP1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LCSUEWSP1_API ALcsUeWsP1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
